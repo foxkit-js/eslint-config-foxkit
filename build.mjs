ba193268e6d806e15badb6f95ea841b9abd68f52
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-06-12 15:26:24 +0200

    change: stop bundling in build and move everything to src dir

--- build.mjs
+++ build.mjs
@@ -1,9 +1,9 @@
-import { rm, cp } from "fs/promises";
+import { relative, join } from "path";
+import { rm, cp, readdir } from "fs/promises";
 import * as esbuild from "esbuild";
 
 const config = {
-  entryPoints: ["configs/index.js", "configs/ts.js", "configs/react.js"],
-  bundle: true,
+  bundle: false,
   platform: "node",
   packages: "external",
   outdir: "dist",
@@ -11,14 +11,34 @@ const config = {
   target: "node16"
 };
 
+const __dirname = new URL(".", import.meta.url).pathname.slice(0, -1);
+
+async function discoverDirectory(dirPath) {
+  const dir = await readdir(dirPath);
+  return dir
+    .filter(str => str.endsWith(".js"))
+    .map(file => relative(__dirname, join(dirPath, file)));
+}
+
 async function main() {
   console.log("Cleaning");
   await rm("dist", { recursive: true, force: true });
+
+  console.log("Discovering source files");
+  const dirs = await Promise.all([
+    discoverDirectory("src"),
+    discoverDirectory("src/configs"),
+    discoverDirectory("src/rules")
+  ]);
+  const entryPoints = dirs.flat();
+
   console.log("Building bundles");
   await esbuild.build({
     ...config,
+    entryPoints,
     format: "cjs"
   });
+
   console.log("Copying README.md");
   await cp("./README.md", "dist/README.md", { force: true });
   console.log("Completed");
