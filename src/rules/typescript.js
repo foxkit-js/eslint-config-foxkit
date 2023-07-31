1d956a1f13f4a1ad96038aac574c5b0c4998ac45
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-07-31 17:49:39 +0200

    fix: use typescript version of no-use-before-define

--- src/rules/typescript.js
+++ src/rules/typescript.js
@@ -14,7 +14,8 @@ module.exports.baseRules = Object.assign({}, eslintRecommended.rules);
 module.exports.recommendedRules = Object.assign({}, tsRecommended.rules, {
   "prefer-const": "warn", // warn is good enough
   "@typescript-eslint/ban-ts-comment": "off", // just no, refuse
-  "@typescript-eslint/ban-types": "off", // this bans `{}` and says to use `object` instead. `object` incorrectly used Set, Map and such
   "@typescript-eslint/no-non-null-assertion": "off", // is simply needed at times with types from external libs
-  "@typescript-eslint/no-unused-vars": ["warn", { varsIgnorePattern: "^_+" }] // Funfact: the ignore pattern doesn't always work on function params
+  "@typescript-eslint/no-unused-vars": ["warn", { varsIgnorePattern: "^_+" }], // Funfact: the ignore pattern doesn't always work on function params
+  "no-use-before-define": "off", // use typescript version instead
+  "@typescript-eslint/no-use-before-define": ["error", { variables: true }]
 });
